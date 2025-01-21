c2afc01f5f8620156e962826dc5d9f30bf469e16
Craig McLellen <craig.mclellen@insight.com>
2025-01-21 17:41:44 +0800

    Fix

--- Api/Ez.UrlShortener.Application/Queries/GetOriginalUrlQuery.cs
+++ Api/Ez.UrlShortener.Application/Queries/GetOriginalUrlQuery.cs
@@ -12,16 +12,24 @@ namespace Ez.UrlShortener.Application.Queries
         HybridCache hybridCache,
         IShortenedUrlRepository shortenedUrlRepository) : IRequestHandler<GetOriginalUrlQuery, string?>
     {
+        private static readonly Counter<int> CacheHitCounter = AppMeters.ApiMeter.CreateCounter<int>("url_shortener.cache_hit", "Number of cache hits");
         private static readonly Counter<int> RedirectsCounter = AppMeters.ApiMeter.CreateCounter<int>("url_shortener.redirects", "Number of redirects");
         private static readonly Counter<int> FailedRedirectsCounter = AppMeters.ApiMeter.CreateCounter<int>("url_shortener.failed_redirects", "Number of failed redirects");
 
         public async Task<string?> Handle(GetOriginalUrlQuery request, CancellationToken cancellationToken)
         {
+            bool cacheHit = true;
             var originalUrl = await hybridCache.GetOrCreateAsync(request.shortCode, async token =>
             {
                 var originalUrl = await shortenedUrlRepository.GetByShortCodeAsync(request.shortCode);
+                cacheHit = false;
                 return originalUrl?.OriginalUrl;
             });
+
+            if(cacheHit)
+            {
+                CacheHitCounter.Add(1, new KeyValuePair<string, object?>("short_code", request.shortCode));
+            }
 
             if (originalUrl is null)
             {
